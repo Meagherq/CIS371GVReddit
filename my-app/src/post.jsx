592dfa2aa59c307d16fd03de1a2598aa15d07f2e
Nolan G <gustafsn@mail.gvsu.edu>
2019-04-14 04:52:32 -0400

    Voting on Posts
    Voting now updates the totals on a post correctly, and a user is only able to vote once.
    
    It does not yet update the totals for the Users

--- my-app/src/post.jsx
+++ my-app/src/post.jsx
@@ -12,16 +12,19 @@ class Post extends Component {
         super(props);
         this.change = this.change.bind(this)
         this.goHome = this.goHome.bind(this)
+        this.vote = this.vote.bind(this)
         this.state = {
-            postInfo : null
+            postInfo : null,
+            postRef : fire.database().ref("/Posts/"+this.props.postid+"/"),
+            topRef : fire.database().ref()
         }
     }
 
     componentDidMount() {
         //do a firebase query for the post
-        let ref = fire.database().ref("/Posts/"+this.props.postid+"/")
+        // let ref = fire.database().ref("/Posts/"+this.props.postid+"/")
         // this.setState({postInfo : ref})
-        ref.on('value', (snapshot =>{
+        this.state.postRef.on('value', (snapshot =>{
             this.setState({postInfo : snapshot.val()})
         }))
     }
@@ -38,15 +41,77 @@ class Post extends Component {
         ReactDOM.render(<PostPage postid="templateID" username={this.props.username}/>, document.getElementById("root"));
     }    
     
+    vote(e){
+        let yaynay = e.target.value
+        let currentuid = fire.auth().currentUser.uid
+        let userRef = this.state.topRef.child("/Users/"+currentuid)
+        // userRef.child("/votesMade/").equalTo(this.props.postid).once("value", snapshot =>{
+        userRef.child("/votesMade/"+this.props.postid).once("value", snapshot =>{
+            if(snapshot.exists()){ //vote has already been made
+                //previous vote for this post is 'up'
+                if(snapshot.val() === 'up'){ 
+                    if(yaynay === 'up'){    //pressed 'up' again, so cancel last vote
+                        userRef.child("/votesMade/").update({[this.props.postid] : null})//cancel by changing opinion in User table to null
+                        let votesUp = this.state.postInfo.upvotes
+                        votesUp--   //adjust post's upvotes
+                        this.state.postRef.update({upvotes : votesUp})
+                    }
+                    if(yaynay === 'down'){
+                        userRef.child("/votesMade/").update({[this.props.postid] : 'down'})//change opinion in User table to 'down'
+                        let votesDown = this.state.postInfo.downvotes
+                        let votesUp = this.state.postInfo.upvotes
+                        votesUp--   //adjust upvotes
+                        votesDown++ //adjust downvotes
+                        this.state.postRef.update({upvotes : votesUp})
+                        this.state.postRef.update({downvotes : votesDown})
+                    }
+                }
+                //previous was 'down'
+                if(snapshot.val() === 'down'){ 
+                    if(yaynay === 'down'){    //pressed 'down' again, so cancel last vote
+                        userRef.child("/votesMade/").update({[this.props.postid] : null})//cancel by changing opinion in User table to null
+                        let votesDown = this.state.postInfo.downvotes
+                        votesDown--   //adjust post's downvotes
+                        this.state.postRef.update({downvotes : votesDown})
+                    }
+                    if(yaynay === 'up'){
+                        userRef.child("/votesMade/").update({[this.props.postid] : 'up'})//change opinion in User table to 'up'
+                        let votesUp = this.state.postInfo.upvotes
+                        let votesDown = this.state.postInfo.downvotes
+                        votesUp++ //adjust upvotes
+                        votesDown-- //adjust downvotes
+                        this.state.postRef.update({upvotes : votesUp})
+                        this.state.postRef.update({downvotes : votesDown})
+                    }
+                }
+            }
+            else{   //there hasn't been a vote made yet
+                if(yaynay === 'up'){
+                    userRef.child("/votesMade/").update({[this.props.postid] : 'up'})//change opinion in User table to 'up'
+                    let votesUp = this.state.postInfo.upvotes
+                    votesUp++
+                    this.state.postRef.update({upvotes : votesUp})
+                }
+                if(yaynay === 'down'){
+                    userRef.child("/votesMade/").update({[this.props.postid] : 'down'})//change opinion in User table to 'up'
+                    let votesDown = this.state.postInfo.downvotes
+                    votesDown++
+                    this.state.postRef.update({downvotes : votesDown})
+                }
+            }
+        })
+        
+    }
+
     render() {
         if (this.state.postInfo !== null) {
             return <div className="postTile">
                     {/* <button onClick={this.goHome}>Home</button> */}
                     <div className="votebox">
                         {/* TODO: add functionality for the buttons below */}
-                        <button value="upvote">^</button>
+                        <button value="up" onClick={this.vote}>^</button>
                         <span>{this.state.postInfo.upvotes - this.state.postInfo.downvotes}</span>
-                        <button value="downvote">v</button>
+                        <button value="down" onClick={this.vote}>v</button>
                     </div>
                     <div className="postdata">
                         <span className="author">{"Posted by: "+this.state.postInfo.userID}</span><br/>
